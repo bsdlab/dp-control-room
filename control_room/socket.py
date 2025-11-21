90a9c050709a0d372b6f5a9c93a81c34c7b8a573
Alexander de Ranitz <alexanderderanitz@gmail.com>
2025-11-21 12:14:49 +0100

    Use short timeout in creating socket, returning TimeoutError if creation timed out
    If the connection is not possible, we want to fail fast. If we don't pass a timeout, the default behaviour is waiting up to 2s, which means this whole function takes > 6s even if the wait between retries is short.
    
    In case we want to wait a while between retries to allow for a slow startup, this can be controlled by the retry_connection_after_s parameter.

--- control_room/socket.py
+++ control_room/socket.py
@@ -8,13 +8,13 @@ MAX_CONNECT_RETRIES = 3
 
 
 def create_socket_client(
-    host_ip: str, port: int, retry_connection_after_s: float = 1
+    host_ip: str, port: int, retry_connection_after_s: float = 1, timeout: float = 0.1
 ) -> socket.socket:
     """
     Create a socket client and attempt to connect to a specified host and port.
 
     This function attempts to establish a TCP connection to the specified host and port.
-    It retries the connection up to a maximum number of times (3) if the connection is refused.
+    It retries the connection up to a maximum number of times (3) if the connection is refused or times out.
     If the connection is successful, the socket object is returned.
 
     Parameters
@@ -25,6 +25,8 @@ def create_socket_client(
         The port number on the host to connect to.
     retry_connection_after_s : float, optional
         The number of seconds to wait between connection attempts, by default 1.
+    timeout : float, optional
+        The timeout duration for the socket connection in seconds, by default 0.1.
 
     Returns
     -------
@@ -40,8 +42,7 @@ def create_socket_client(
     while conn_try < MAX_CONNECT_RETRIES:
         try:
             logger.debug(f"Trying connection to: {host_ip=}, {port=}")
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((host_ip, port))
+            s = socket.create_connection((host_ip, port), timeout=timeout)
             logger.debug(f"Connected to: {host_ip=}, {port=} using {s=}")
             return s
         except ConnectionRefusedError:
@@ -51,10 +52,31 @@ def create_socket_client(
             # we will get a an Errno 22 in the second try
             s.close()
 
+            if conn_try == MAX_CONNECT_RETRIES - 1:
+                raise ConnectionRefusedError(
+                    f"Creating socket failed. Connection refused after {MAX_CONNECT_RETRIES} tries: {host_ip=}, {port=}"
+                )
             time.sleep(retry_connection_after_s)
-            pass
-        conn_try += 1
+        except TimeoutError:
+            logger.debug(
+                f"Connection timed out: {host_ip=}, {port=}, try={conn_try + 1}"
+            )
+            time.sleep(retry_connection_after_s)
 
-    raise ConnectionRefusedError(
-        f"Connection refused after {MAX_CONNECT_RETRIES} tries: {host_ip=}, {port=}"
-    )
+            if conn_try == MAX_CONNECT_RETRIES - 1:
+                raise TimeoutError(
+                    f"Creating socket failed. Connection timed out after {MAX_CONNECT_RETRIES} tries: {host_ip=}, {port=}"
+                )
+        except Exception as e:
+            logger.error(f"Unexpected error when creating socket: {e}")
+            try:
+                s.close()
+            except:
+                pass
+
+            if conn_try == MAX_CONNECT_RETRIES - 1:
+                raise type(e)(
+                    f"Creating socket failed. Unknown error after {MAX_CONNECT_RETRIES} tries: {host_ip=}, {port=}"
+                ) from e
+            time.sleep(retry_connection_after_s)
+        conn_try += 1
